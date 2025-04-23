13c1363e529700471801b5392d1cfb21d741fbba
aakksshhiitt <mailakshithere@gmail.com>
2025-04-23 10:17:01 +0530

    Add files via upload

--- preSale.sol
+++ preSale.sol
@@ -1,105 +1,193 @@
 // SPDX-License-Identifier:MIT
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity^0.8.28;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-// Contract to acepting USDC and transferring the MY Token
-contract preSale is Ownable{
+contract presale{
 
-    address public Owner;
-    IERC20 USDContract;
-    IERC20 MyToken;    
-    uint256 vestingTime;                  //time for which tokens are locked after purchase.  
-    uint256 tokensSold;                                    
-    uint256 totalTokenAvailableforSale;
-    uint256 tempData;
-    mapping(address=>address) referred;
-    mapping(address=>uint256) public tokensPurchased;
-    mapping(address=>uint256) public tokenWithdrawn;
-    mapping(address=>uint256) buyTime;       //time at which USDC is sent buy a particular address.
+// declaring state variables
+    address public owner;
+    uint256 public totalTokensForSale;
+    uint256 public tokensLeftForSale;
+    uint256 public vestingPeriod;
+    uint256 public tokensSold;
+    uint256 public ownerFunds;
+    bool public USDCAddressSet;
+    IERC20 RewardToken;     // Contract address for the reward token
+    IERC20 public USDCToken;      // contract address for the USDC token to make the payment
 
+    mapping(address=>uint256) public stakedAmount;
+    mapping(address=>uint256) public stakedTime;
+    mapping(address=>uint256) public rewardTokensOf;
+    mapping(address=>bool) public rewardsClaimed;
+    mapping(address=>address) public referred;
 
-    event TokensLocked(address indexed _buyer,uint256 indexed _tokenValue);
-    event ReferralProcessed(uint256 indexed _USDCAmount,address _buyer);
-    event WithdrawlDone(address indexed _buyer,uint256 _amount);
-
-    constructor(IERC20 _USDCAddress, IERC20 MyTokenAddress, uint256 _vestingTime) Ownable(msg.sender){
-        USDContract=_USDCAddress;
-        MyToken=MyTokenAddress;
-        totalTokenAvailableforSale=MyToken.totalSupply();
-        vestingTime=_vestingTime;
-        Owner=owner();
+    constructor(uint256 _rewardTokenSupply, uint256 _totalTokensForSale, uint256 _vestingPeriod){
+        owner=msg.sender;
+        totalTokensForSale=_totalTokensForSale;
+        tokensLeftForSale=_totalTokensForSale;
+        vestingPeriod=_vestingPeriod;
+        tokensSold=0;
+        USDCAddressSet=false;
+        
+        RewardCoin r=new RewardCoin(_rewardTokenSupply);  // initiating the Reward Coin contract
+        RewardToken=r;
     }
 
-// function to send USDC and get the MT token
-
-    function purchase(uint256 _USDCAmount) public{
-
-        require(msg.sender!=address(0),"Address(0) address can't buy token.");
-        require(tokensPurchased[msg.sender]==0,"Sorry one use can purchase sale tokens once");
-        uint256 requiredTokens=requiredMyTokens(_USDCAmount);
-        // require(requiredTokens < 4000 ether,"Sorry one use can only buy at most of 4000 tokens");
-        require(requiredTokens<=totalTokenAvailableforSale,"Sorry less tokens are available for sale.");
-        require(USDContract.allowance(msg.sender,address(this))>=_USDCAmount,"Sorry you are not allowed to make this transfer");
-
-        tokensSold+=requiredTokens;
-        buyTime[msg.sender]=block.timestamp;                              //time at which tokens are bought
-        totalTokenAvailableforSale-=requiredTokens;
-        USDContract.transferFrom(msg.sender,address(this),_USDCAmount);
-        tokensPurchased[msg.sender]+=requiredTokens;                      //these tokens will be unlocked after vesting period
+    modifier onlyOwner{
+        require(msg.sender==owner,"Only owner can access this functionality");
+        _;
+    }
+    modifier onlyBuyer{
+        require(stakedAmount[msg.sender]>0,"Only the users that have vested in the token can access this functionality");
+        _;
     }
 
-// Function for the referral purchase where user will pass the USDC amount and the address who has referred him.
-    function referralPurchaseAndLock(uint256 _USDCAmount,address _referralAddress) public{
-        require(tokensPurchased[_referralAddress]>0,"Sorry the referral address need to be the owner of the MyTokens");
+// function to set the contract address of the USDC contract that will be staked.
+// USDC contract can be set only once.
+    function SetUSDCContract(IERC20 _USDCAddress) public onlyOwner{
+        require(USDCAddressSet==false,"Sorry, the USDC token address is already set");
+        USDCToken=_USDCAddress;
+        USDCAddressSet=true;
+    }
+
+// function for everyone to stake the usdc after approving the contract to tramsfer the specific USDC tokens.
+    function purchaseAndLock(uint256 _USDCAmount) public{
+        require(msg.sender!=address(0),"You are not allowed to stake the tokens");
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can stake");
+        require(_USDCAmount>0,"Please stake some USDC");
+        require(stakedAmount[msg.sender]==0,"Sorry you have already staked the USDC");
+        require(rewardsClaimed[msg.sender]==false,"You have already claimed the rewards");
+        require(USDCToken.allowance(msg.sender, address(this))>=_USDCAmount,"Please provide the allowance to the contract");
+        require(USDCToken.balanceOf(msg.sender)>=_USDCAmount,"Sorry, you do not have the required USDC tokens");
+        uint256 requiredRewardTokens=calculateRewardTokens(_USDCAmount);
+        require(tokensLeftForSale>=requiredRewardTokens,"Sorry less tokens are left for sale, try to stake less amount");
+        
+        rewardTokensOf[msg.sender]=requiredRewardTokens;
+        tokensLeftForSale-=requiredRewardTokens;
+        tokensSold+=requiredRewardTokens;
+        stakedAmount[msg.sender]=_USDCAmount;
+        stakedTime[msg.sender]=block.timestamp;
+        USDCToken.transferFrom(msg.sender,address(this),_USDCAmount);
+    }
+
+// function for the users to purchase the tokens and refer the user that has already claimed the rewards.
+// referral rewards will be till 3 hierarchical level, ie. 10%, 5%, 2%
+    function referralPurchaseLock(uint256 _USDCAmount, address _referralAddress) public{
+        require(rewardsClaimed[_referralAddress]==true,"Sorry, the referral address has not claimed the tokens yet");
         require(_referralAddress!=address(0),"Please provide a valid address");
+        purchaseAndLock(_USDCAmount);
+        referred[msg.sender]=_referralAddress;
+    }
 
-        purchase(_USDCAmount);
-        referred[msg.sender]=_referralAddress;                      // storing the address of the user who has referred the msg.sender
-        emit TokensLocked(msg.sender, tokensPurchased[msg.sender]);
-    }    
+// function for the buyers to withdraw the staked amount before the vesting period.
+    function withdrawBeforeTime() public onlyBuyer{
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        require(stakedAmount[msg.sender]>0,"Sorry, you do not have staked any amount");
+        // require(block.timestamp<stakedTime[msg.sender] + vestingPeriod * 24*60*60,"You have completed the staking time, click on claimVesting rewards and get the reward tokens");
 
-// function to get the MT tokens in exchange of USDC Amount according to the Sale Price Phase
-    function requiredMyTokens(uint256 _USDCAmount) internal view returns(uint256){
-        uint256 priceDecider=1+(tokensSold/(25000 ether));     // price decider will be 1 for 0-25k, 2 for 25k-50k, 3 for 50k-75k, 4 for 75k-100k tokens sold.
+        uint256 refundAmount=stakedAmount[msg.sender];
+        tokensLeftForSale+=rewardTokensOf[msg.sender];
+        tokensSold-=rewardTokensOf[msg.sender];
+        rewardTokensOf[msg.sender]=0;
+        stakedAmount[msg.sender]=0;
+        stakedTime[msg.sender]=0;
+        
+        USDCToken.transfer(msg.sender, refundAmount);
+    }
+
+// function for the buyers to clim the reward after the vesting period is over.
+    function claimRewards() public{
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        require(rewardsClaimed[msg.sender]==false,"Sorry, you have already claimed the rewards");
+        require(stakedAmount[msg.sender]>=0,"Sorry, you haven't staked any funds");
+        require(stakedAmount[msg.sender]>0,"Only the users that have vested in the token can access this functionality");
+
+        // require(block.timestamp>=stakedTime[msg.sender] + vestingPeriod * 24*60*60, "Sorry, the staking time is not completed yet, you can only claim the rewards after the staking period of 100 days is over");
+        
+        uint256 rewardAmount=rewardTokensOf[msg.sender];
+        address previousRefferedAddress=referred[msg.sender];
+        uint8 percentage=10;
+
+        ownerFunds+=stakedAmount[msg.sender];
+        rewardTokensOf[msg.sender]=0;
+        stakedAmount[msg.sender]=0;
+        stakedTime[msg.sender]=0;
+        rewardsClaimed[msg.sender]=true;
+
+        RewardToken.transfer(msg.sender, rewardAmount);
+        for(uint8 i=0;i<3;i++){
+            if(previousRefferedAddress!=address(0)){
+                RewardToken.transfer(previousRefferedAddress,(percentage*rewardAmount)/100);
+            }
+            else break;
+            percentage=percentage/2;
+            previousRefferedAddress=referred[previousRefferedAddress];
+        }
+    }
+
+// function for the Owner to withdraw the USDC amount staked and claimed
+    function withdrawUSDC() public onlyOwner{
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        require(ownerFunds>0,"Sorry, no fund is available to be withdrawn");
+        USDCToken.transfer(owner,ownerFunds);
+        ownerFunds=0;
+    }
+
+// function(Internal) to calculate the reward tokens that will be given to stake _USDCAmount.
+    function calculateRewardTokens(uint256 _USDCAmount) internal view returns(uint256){
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        uint256 priceDecider=1 + (tokensSold/250000 ether);
         if(priceDecider>4){
             priceDecider=4;
         }
-        return ((4*_USDCAmount*10**18)/(10**6))/priceDecider;
+        return (4*_USDCAmount*10**18)/((10**6)*priceDecider);
     }
 
-//function for the buyers to unlock their tokens and referral bonus after the vesting period is over
-    function withdrawVestingFunds() public{
-        // require(block.timestamp > buyTime[msg.sender]+vestingTime,"Sorry tokens are locked till the vesting period ends");
-        require(tokensPurchased[msg.sender]>0,"Sorry you haven't locked any tokens till yet.");
-
-        tempData=tokensPurchased[msg.sender];  // amount of  tokens locked.
-        tokensPurchased[msg.sender]=0;
-        tokenWithdrawn[msg.sender]+=tempData;
-        MyToken.transferFrom(Owner,msg.sender, tempData);
-
-        address previousReferredAddress=referred[msg.sender];
-        uint8 variable=10;
-        for(uint i=0;i<3;i++){
-            if(previousReferredAddress!=address(0)){
-                MyToken.transferFrom(Owner, previousReferredAddress,(variable*tempData/100));
-            }
-            else break;
-            variable=variable/2;  //variable will be 10=>5=>2 for 3 intervals
-            previousReferredAddress=referred[previousReferredAddress];
-        }
-        emit WithdrawlDone(msg.sender,tempData);
+// function for the buyers to check their vestedAmount.
+    function checkMyVestedAmount() public view returns(uint256){
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        return stakedAmount[msg.sender];
     }
 
-// Function for owner to withdraw USDC at desired address
-    function withdrawOwnerFunds() public onlyOwner{
-        require(msg.sender==Owner," Only owner can withdraw the deposited USDC amount");
-        USDContract.transfer(msg.sender,USDContract.balanceOf(address(this)));
+// function for everyone to check the reward tokens that will be given to stake _USDCAmount.
+    function checkRewardAmount(uint256 _USDCAmount) public view returns(uint256){
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        return calculateRewardTokens(_USDCAmount);
     }
 
-// function to check for the tokens left for sale.
-    function totalTokensForSale() public view returns(uint256){
-        return totalTokenAvailableforSale;
+    function myRewardBalance() public view returns(uint256){
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        return RewardToken.balanceOf(msg.sender);
+    }
+
+    function timeLeft() public view onlyBuyer returns(uint256){
+        require(USDCAddressSet==true,"Please let the owner set the USDC adderss first so that you can access the functionality");
+        if(vestingPeriod*60*60*24-(block.timestamp-stakedTime[msg.sender])>0)
+        return vestingPeriod*60*60*24-(block.timestamp-stakedTime[msg.sender]);
+        else
+        return 0;
     }
 
 }
+
+// contract of the Reward Coin that will be automatically deployed by the constructor of the presale contract.
+contract RewardCoin is ERC20, Ownable {
+    address initialOwner;
+    uint256 initialSupply;
+    constructor(uint256 _totalSupply) ERC20("RewardToken","RT") Ownable(msg.sender){
+        _mint(msg.sender ,_totalSupply);  // transferring the tokens to the calling contract
+    }
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+}
+
+//user can stake the USDC funds  for 100 days and get the reward tokens
+// user can withdraw the staked amount USDC within 100 days
+// AFter 100 days of staking user can only get the reward tokens he is eligible for.
+// owner can only withdraw the USDC for which the rewards have been given.
+// for first 250000 reward tokens 1USDC=4Reward tokens, for reward tokens from 250k-500k 1USDC=2 Reward tokens, for 500k-750k 1USDC=4/3 reward tokens and further 1USDC=1Reward token
+// referral address should have claimed the rewards before.
+// referral rewards will be till 3 hierarchical level, ie. 10%, 5%, 2%
+// The person that has claimed hte rewars once is not allowed to purchase the reward tokens again.
